a7299346b896c7a9c5714cbdc2a0f4802f40a845
Nicolas Zamorano <81661367+Nicolas-Zamorano@users.noreply.github.com>
2025-06-10 11:57:20 +0200

    FIx elements4edges
    FIx elements4boundary_edges  to be computed correctly, due to previus error related to new shape for nodes4elements

--- src/fem.py
+++ src/fem.py
@@ -32,13 +32,14 @@ class Abstract_Mesh(ABC):
                                                                     return_counts = True, 
                                                                     dim = -1)
         
-        nodes4unique_edges = nodes4unique_edges.mT
+        nodes4unique_edges = nodes4unique_edges.mT.unsqueeze(-2)
                 
         nodes4boundary_edges = nodes4unique_edges[boundary_mask == 1]
         nodes4inner_edges = nodes4unique_edges[boundary_mask != 1]
         
-        elements4boundary_edges = (nodes4boundary_edges.unsqueeze(-2).unsqueeze(-2) == nodes4elements.unsqueeze(-3).unsqueeze(-1)).any(dim = -2).all(dim = -1).float().argmax(dim = -1) 
         elements4inner_edges = torch.nonzero((nodes4inner_edges.unsqueeze(-2).unsqueeze(-2) == nodes4elements.unsqueeze(-3).unsqueeze(-1)).any(dim = -2).all(dim = -1))[:, 1].reshape(-1, mesh_parameters["nb_dimensions"])
+        elements4boundary_edges = (nodes4boundary_edges.unsqueeze(-2) == nodes4elements.unsqueeze(-4).mT).any(dim = -2).all(dim = -1).float().argmax(dim = -1,keepdim = True) 
+        elements4inner_edges = torch.nonzero((nodes4inner_edges.unsqueeze(-2) == nodes4elements.unsqueeze(-4).mT).any(dim = -2).all(dim = -1))[:, 1].reshape(-1, mesh_parameters["nb_dimensions"])
         
         nodes4boundary = torch.unique(nodes4boundary_edges)
         nodes_idx4boundary_edges = torch.nonzero((nodes4unique_edges.unsqueeze(-2) == nodes4boundary_edges.unsqueeze(-3)).all(dim = -1).any(dim = -1))
