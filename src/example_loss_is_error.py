94a9cc540d365e6165d14601e32544c20b47b08b
Nicolas Zamorano <nicolas.zamo12@gmail.com>
2025-09-09 17:01:16 +0200

    update examples

--- src/example_loss_is_error.py
+++ src/example_loss_is_error.py
@@ -2,309 +2,164 @@
 equation with Dirichlet boundary conditions."""
 
 import math
-from datetime import datetime
 
 import matplotlib.pyplot as plt
+import tensordict as td
 import torch
-from Triangulation import Triangulation
+import triangle as tr
 
 from fem import Basis, ElementTri, MeshTri
-from neural_network import NeuralNetwork
+from model import FeedForwardNeuralNetwork as NeuralNetwork, Model
 
-torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
-torch.cuda.empty_cache()
+# torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
+# torch.cuda.empty_cache()
 torch.set_default_dtype(torch.float64)
-torch.manual_seed(1234)
 
 # ---------------------- Neural Network Parameters ----------------------#
 
 
-def nn_gradient(neural_net, x, y):
-    """Compute gradient of a Neural Network w.r.t inputs."""
-    x.requires_grad_(True)
-    y.requires_grad_(True)
-
-    output = neural_net.forward(x, y)
-
-    gradients = torch.autograd.grad(
-        outputs=output,
-        inputs=(x, y),
-        grad_outputs=torch.ones_like(output),
-        retain_graph=True,
-        create_graph=True,
-    )
-
-    return torch.concat(gradients, dim=-1)
+def boundary_constraint(x):
+    """Boundary condition modifier function."""
+    return x[..., [0]] * (x[..., [0]] - 1) * x[..., [1]] * (x[..., [1]] - 1)
 
 
-def nn_laplacian(neural_net, x, y):
-    """
-    Compute Laplacian of a Neural Network w.r.t inputs.
-
-    Parameters:
-    - inputs (torch.Tensor): input tensors.
-
-    Returns:
-    - laplacian (torch.Tensor): The Laplacian of the network output w.r.t each input tensor.
-    """
-
-    x.requires_grad_(True)
-    y.requires_grad_(True)
-
-    output = neural_net.forward(x, y)
-
-    gradients = torch.autograd.grad(
-        outputs=output,
-        inputs=(x, y),
-        grad_outputs=torch.ones_like(output),
-        retain_graph=True,
-        create_graph=True,
-    )
-
-    du_xx = torch.autograd.grad(
-        outputs=gradients[0],
-        inputs=(x, y),
-        grad_outputs=torch.ones_like(gradients[0]),
-        retain_graph=True,
-        create_graph=True,
-    )[0]
-
-    du_yy = torch.autograd.grad(
-        outputs=gradients[1],
-        inputs=(x, y),
-        grad_outputs=torch.ones_like(gradients[1]),
-        retain_graph=True,
-        create_graph=True,
-    )[1]
-
-    lap = du_xx + du_yy
-
-    return lap
-
-
-def optimizer_step(opt, value_loss):
-    """Perform an optimization step."""
-    opt.zero_grad()
-    value_loss.backward(retain_graph=True)
-    opt.step()
-    scheduler.step()
-
-
-EPOCHS = 1000
-LEARNING_RATE = 0.5e-1
-DECAY_RATE = 0.95
-DECAY_STEPS = 100
-
-NN = torch.jit.script(
-    NeuralNetwork(
-        input_dimension=2, output_dimension=1, deep_layers=5, hidden_layers_dimension=10
-    )
-)
-
-optimizer = torch.optim.Adam(NN.parameters(), lr=0.1e-2)
-
-scheduler = torch.optim.lr_scheduler.ExponentialLR(
-    optimizer, DECAY_RATE ** (1 / DECAY_STEPS)
+NN = NeuralNetwork(
+    input_dimension=2,
+    output_dimension=1,
+    nb_hidden_layers=4,
+    neurons_per_layers=25,
+    boundary_condition_modifier=boundary_constraint,
 )
 
 # ---------------------- FEM Parameters ----------------------#
 
-c4n, n4e = Triangulation(3)
+mesh_data = tr.triangulate(
+    {"vertices": [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]]},
+    "Dqea" + str(0.5**10),
+)
 
-coords4nodes = torch.tensor(c4n)
+mesh_data_torch = td.TensorDict(mesh_data)
 
-nodes4elements = torch.tensor(n4e)
+mesh = MeshTri(triangulation=mesh_data_torch)
 
-mesh = MeshTri(coords4nodes, nodes4elements)
+elements = ElementTri(polynomial_order=1, integration_order=4)
 
-mesh = MeshTri(coords4nodes, nodes4elements)
-
-elements = ElementTri(polynomial_order=1, integration_order=2)
-
-V = Basis(mesh, elements)
-
-# ---------------------- Residual Parameters ----------------------#
-
-
-def rhs(x, y):
-    """Right-hand side function."""
-    return -2 * math.pi**2 * torch.sin(math.pi * x) * torch.sin(math.pi * y)
-
-
-def residual(basis):
-    """Compute the residual of the Poisson equation using the neural network as an approximation."""
-    x, y = basis.integration_points
-
-    # NN_grad = NN_gradient(NN, x, y)
-    v = basis.v
-    # v_grad = basis.v_grad
-    rhs_value = rhs(x, y)
-    nn_lap = nn_laplacian(NN, x, y)
-
-    return (nn_lap + rhs_value(x, y)) * v
-
-    # return NN_grad @ v_grad.mT - rhs_value * v
-
-
-def gram_matrix(basis):
-    """Compute the Gram matrix for the basis functions."""
-    v = basis.v
-    v_grad = basis.v_grad
-
-    return 3 * v_grad @ v_grad.mT + v @ v.mT
-    # return 3 * v_grad @ v_grad.mT
-    # return v @ v.mT
-
-
-A = V.integrate_bilinear_form(gram_matrix)
-
-A_inv = torch.inverse(V.integrate_bilinear_form(gram_matrix))
+discrete_basis = Basis(mesh, elements)
 
 # ---------------------- Error Parameters ----------------------#
 
 
+def exact(x, y):
+    """Exact solution of the PDE."""
+    return torch.sin(math.pi * x) * torch.sin(math.pi * y)
+
+
+def exact_dx(x, y):
+    """Exact solution derivative with respect to x."""
+    return math.pi * torch.cos(math.pi * x) * torch.sin(math.pi * y)
+
+
+def exact_dy(x, y):
+    """Exact solution derivative with respect to y."""
+    return math.pi * torch.sin(math.pi * x) * torch.cos(math.pi * y)
+
+
 def h1_exact(basis):
-    """Compute the H1 norm of the exact solution."""
-    x, y = basis.integration_points
+    """H1 norm of the exact solution."""
+    x, y = torch.split(basis.integration_points, 1, dim=-1)
 
-    exact = (torch.sin(math.pi * x) * torch.sin(math.pi * y)) / (2 * math.pi**2)
-
-    exact_dx = (torch.cos(math.pi * x) * torch.sin(math.pi * y)) / (2 * math.pi)
-    exact_dy = (torch.sin(math.pi * x) * torch.cos(math.pi * y)) / (2 * math.pi)
-
-    return (exact_dx**2 + exact_dy**2) ** 2 + exact**2
+    return exact(x, y) ** 2 + exact_dx(x, y) ** 2 + exact_dy(x, y) ** 2
 
 
-def h1_norm(basis):
-    """Compute the H1 norm of the neural network solution."""
-    x, y = basis.integration_points
+def h1_norm(basis, neural_network, gradient):
+    """H1 norm of the neural network solution."""
+    integration_points = basis.integration_points
+    x, y = torch.split(integration_points, 1, dim=-1)
 
-    nn_dx, nn_dy = torch.split(nn_gradient(NN, x, y), 1, dim=-1)
+    nn_dx, nn_dy = torch.split(gradient(integration_points), 1, dim=-1)
 
-    exact = (torch.sin(math.pi * x) * torch.sin(math.pi * y)) / (2 * math.pi**2)
-
-    exact_dx = (torch.cos(math.pi * x) * torch.sin(math.pi * y)) / (2 * math.pi)
-    exact_dy = (torch.sin(math.pi * x) * torch.cos(math.pi * y)) / (2 * math.pi)
-
-    l2_error = (exact - NN(x, y)) ** 2
-
-    h1_0_error = (exact_dx - nn_dx) ** 2 + (exact_dy - nn_dy) ** 2
-
-    return l2_error + h1_0_error
+    return (
+        (exact(x, y) - neural_network(integration_points)) ** 2
+        + (exact_dx(x, y) - nn_dx) ** 2
+        + (exact_dy(x, y) - nn_dy) ** 2
+    )
 
 
-exact_norm = torch.sqrt(torch.sum(V.integrate_functional(h1_exact)))
-
-loss_list = []
-relative_loss_list = []
-H1_error_list = []
-
-LOSS_OPT = 10e4
-PARAMS_OPT = NN.state_dict()
+exact_norm = torch.sqrt(torch.sum(discrete_basis.integrate_functional(h1_exact)))
 
 # ---------------------- Training ----------------------#
 
-start_time = datetime.now()
 
-for epoch in range(EPOCHS):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"{'='*20} [{current_time}] Epoch:{epoch + 1}/{EPOCHS} {'='*20}")
-
-    # residual_value = V.integrate_lineal_form(residual)
-
-    # loss_value = residual_value.T @ (A_inv @ residual_value)
-
-    loss_value = torch.sqrt(torch.sum(V.integrate_functional(h1_norm)))
-
-    optimizer_step(optimizer, loss_value)
-
-    error_norm = torch.sqrt(torch.sum(V.integrate_functional(h1_norm))) / exact_norm
-
-    # relative_loss = torch.sqrt(loss_value)/exact_norm
-
-    relative_loss = loss_value / exact_norm
-
-    print(
-        f"Loss: {loss_value.item():.8f} Relative Loss: {relative_loss.item():.8f} Relative error: {error_norm.item():.8f}"
+def training_step(neural_network):
+    """Training step for the neural network."""
+    loss_value = torch.sum(
+        discrete_basis.integrate_functional(
+            h1_norm, neural_network, neural_network.gradient
+        )
     )
 
-    if loss_value < LOSS_OPT:
-        LOSS_OPT = loss_value
-        PARAMS_OPT = NN.state_dict()
+    relative_loss = torch.sqrt(loss_value) / exact_norm**2
 
-    loss_list.append(loss_value.item())
-    relative_loss_list.append(relative_loss.item())
-    H1_error_list.append(error_norm.item())
+    h1_error = torch.sqrt(
+        torch.sum(
+            discrete_basis.integrate_functional(
+                h1_norm, neural_network, neural_network.gradient
+            )
+        )
+    )
 
-end_time = datetime.now()
+    return loss_value, relative_loss, h1_error / exact_norm
 
-execution_time = end_time - start_time
 
-print(f"Training time: {execution_time}")
+model = Model(
+    neural_network=NN,
+    training_step=training_step,
+    epochs=5000,
+    learning_rate=0.1e-1,
+    use_decay_learning_rate=True,
+    decay_rate=0.99,
+    decay_steps=100,
+    use_early_stopping=True,
+    early_stopping_patience=100,
+    min_delta=1e-12,
+)
+
+model.train()
 
 # ---------------------- Plotting ----------------------#
 
-NN.load_state_dict(PARAMS_OPT)
+model.load_optimal_parameters()
 
-NB_PLOT_POINTS = 100
+linspace = torch.linspace(0, 1, 100)
 
-X, Y = torch.meshgrid(
-    torch.linspace(0, 1, NB_PLOT_POINTS),
-    torch.linspace(0, 1, NB_PLOT_POINTS),
-    indexing="ij",
+X, Y = torch.stack(
+    torch.meshgrid(
+        linspace,
+        linspace,
+        indexing="ij",
+    )
 )
+
+plot_points = torch.stack([X, Y], dim=-1)
 
 with torch.no_grad():
-    Z = NN(X, Y)
+    Z = abs(exact(X, Y) - NN(plot_points).squeeze(-1))
 
-figure_solution = plt.figure()
-axis_solution = figure_solution.add_subplot(111, projection="3d")
-
-contour = axis_solution.plot_surface(
-    X.cpu().detach().numpy(),
-    Y.cpu().detach().numpy(),
-    Z.cpu().detach().numpy(),
+figure_solution, axis_solution = plt.subplots()
+contour_solution = axis_solution.contourf(
+    X.numpy(force=True),
+    Y.numpy(force=True),
+    Z.numpy(force=True),
+    levels=100,
     cmap="viridis",
 )
+figure_solution.colorbar(contour_solution, ax=axis_solution, orientation="vertical")
 
-axis_solution.set(
-    title="Solution obtain with RVPINNs method",
-    xlabel="x",
-    ylabel="y",
-    zlabel=r"$u_\theta(x,y)$",
-)
+axis_solution.set_xlabel("x")
+axis_solution.set_ylabel("y")
+axis_solution.set_title(r"$|u-u_\theta|$")
+plt.tight_layout()
 
-figure_loss, axis_loss = plt.subplots()
-
-axis_loss.semilogy(loss_list)
-
-axis_loss.set(
-    title="Error evolution of RVPINNs method", xlabel="# epochs", ylabel="Error"
-)
-
-figure_error, axis_error = plt.subplots()
-
-axis_error.semilogy(
-    relative_loss_list,
-    label=r"$\frac{\sqrt{\mathcal{L}(u_\theta)}}{\|u\|_{V}}$",
-    linestyle="-.",
-)
-
-axis_error.semilogy(
-    H1_error_list, label=r"$\frac{\|u-u_\theta\|_{V}}{\|u\|_{V}}$", linestyle=":"
-)
-
-axis_error.legend()
-
-figure_loglog, axis_loglog = plt.subplots()
-
-axis_loglog.loglog(relative_loss_list, H1_error_list)
-
-axis_loglog.set(
-    title="Error vs Loss comparison of RVPINNs method",
-    xlabel="Relative Loss",
-    ylabel="Relative Error",
-)
-
+model.plot_training_history()
 
 plt.show()
--- src/example_weak.py
+++ src/example_weak.py
@@ -1,79 +1,41 @@
 "# Example of solving a Poisson equation using a neural network and FEM basis functions."
 
 import math
-from datetime import datetime
+
 import matplotlib.pyplot as plt
 import tensordict as td
 import torch
 import triangle as tr
+
 from fem import Basis, ElementTri, MeshTri
-from neural_network import NeuralNetwork
+from model import FeedForwardNeuralNetwork as NeuralNetwork, Model
 
 # torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
 # torch.cuda.empty_cache()
 torch.set_default_dtype(torch.float64)
 
-# ---------------------- Neural Network Functions ----------------------#
-
-
-def nn_gradient(neural_net, x, y):
-    """Compute the gradient of the neural network output with respect to its inputs."""
-    x.requires_grad_(True)
-    y.requires_grad_(True)
-
-    output = neural_net.forward(x, y)
-
-    gradients = torch.autograd.grad(
-        outputs=output,
-        inputs=(x, y),
-        grad_outputs=torch.ones_like(output),
-        retain_graph=True,
-        create_graph=True,
-    )
-
-    return torch.concat(gradients, dim=-1)
-
-
-def optimizer_step(opt, value_loss):
-    """Perform an optimization step."""
-    opt.zero_grad()
-    value_loss.backward(retain_graph=True)
-    opt.step()
-    scheduler.step()
-
 
 # ---------------------- Neural Network Parameters ----------------------#
 
-EPOCHS = 5000
-LEARNING_RATE = 0.1e-2
-DECAY_RATE = 0.99
-DECAY_STEPS = 100
 
-NN = torch.jit.script(
-    NeuralNetwork(
-        input_dimension=2, output_dimension=1, deep_layers=4, hidden_layers_dimension=25
-    )
+def boundary_constraint(x):
+    """Boundary condition modifier function."""
+    return x[..., [0]] * (x[..., [0]] - 1) * x[..., [1]] * (x[..., [1]] - 1)
+
+
+NN = NeuralNetwork(
+    input_dimension=2,
+    output_dimension=1,
+    nb_hidden_layers=4,
+    neurons_per_layers=25,
+    boundary_condition_modifier=boundary_constraint,
 )
-
-optimizer = torch.optim.Adam(NN.parameters(), lr=LEARNING_RATE)
-
-scheduler = torch.optim.lr_scheduler.ExponentialLR(
-    optimizer, DECAY_RATE ** (1 / DECAY_STEPS)
-)
-
-
-def nn_grad_func(x, y):
-    """Function to compute the gradient of the neural network."""
-    return nn_gradient(NN, x, y)
-
 
 # ---------------------- FEM Parameters ----------------------#
 
-MESH_SIZE = 0.5**8
-
 mesh_data = tr.triangulate(
     {"vertices": [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]]},
-    "Dqea" + str(MESH_SIZE),
+    "Dqena" + str(0.5**2),
 )
 
 mesh_data_torch = td.TensorDict(mesh_data)
@@ -82,11 +44,7 @@ mesh = MeshTri(triangulation=mesh_data_torch)
 
 elements = ElementTri(polynomial_order=1, integration_order=4)
 
-V = Basis(mesh, elements)
-
-fig, ax_mesh = plt.subplots()
-
-# tr.plot(ax_mesh,**mesh_data)
+discrete_basis = Basis(mesh, elements)
 
 # ---------------------- Residual Parameters ----------------------#
 
@@ -98,9 +56,10 @@ def rhs(x, y):
 
 def residual(basis, gradient):
     """Residual of the PDE."""
-    x, y = basis.integration_points
+    integration_points = basis.integration_points
+    x, y = torch.split(integration_points, 1, dim=-1)
 
-    grad = gradient(x, y)
+    grad = gradient(integration_points)
 
     v = basis.v
     v_grad = basis.v_grad
@@ -111,15 +70,14 @@ def residual(basis, gradient):
 
 def gram_matrix(basis):
     """Gram matrix of the basis functions."""
-    v = basis.v
     v_grad = basis.v_grad
 
-    return v_grad @ v_grad.mT + v @ v.mT
+    return v_grad @ v_grad.mT
 
 
-A = V.reduce(V.integrate_bilinear_form(gram_matrix))
-
-A_inv = torch.inverse(A)
+gram_matrix_inverse = torch.inverse(
+    discrete_basis.reduce(discrete_basis.integrate_bilinear_form(gram_matrix))
+)
 
 # ---------------------- Error Parameters ----------------------#
 
@@ -141,119 +99,102 @@ def exact_dy(x, y):
 
 def h1_exact(basis):
     """H1 norm of the exact solution."""
-    x, y = basis.integration_points
+    x, y = torch.split(basis.integration_points, 1, dim=-1)
 
     return exact(x, y) ** 2 + exact_dx(x, y) ** 2 + exact_dy(x, y) ** 2
 
 
-def h1_norm(basis):
+def h1_norm(basis, neural_network, gradient):
     """H1 norm of the neural network solution."""
-    x, y = basis.integration_points
+    integration_points = basis.integration_points
+    x, y = torch.split(integration_points, 1, dim=-1)
 
-    nn_dx, nn_dy = torch.split(nn_gradient(NN, x, y), 1, dim=-1)
+    nn_dx, nn_dy = torch.split(gradient(integration_points), 1, dim=-1)
 
     return (
-        (exact(x, y) - NN(x, y)) ** 2
+        (exact(x, y) - neural_network(integration_points)) ** 2
         + (exact_dx(x, y) - nn_dx) ** 2
         + (exact_dy(x, y) - nn_dy) ** 2
     )
 
 
-exact_norm = torch.sqrt(torch.sum(V.integrate_functional(h1_exact)))
-
-loss_list = []
-relative_loss_list = []
-H1_error_list = []
-
-LOSS_OPT = 10e4
-PARAMS_OPT = NN.state_dict()
+exact_norm = torch.sqrt(torch.sum(discrete_basis.integrate_functional(h1_exact)))
 
 # ---------------------- Training ----------------------#
 
-start_time = datetime.now()
 
-for epoch in range(EPOCHS):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"{'='*20} [{current_time}] Epoch:{epoch + 1}/{EPOCHS} {'='*20}")
-
-    residual_value = V.reduce(V.integrate_linear_form(residual, nn_grad_func))
-
-    loss_value = residual_value.T @ (A_inv @ residual_value)
-
-    # loss_value = (residual_value**2).sum()
-
-    optimizer_step(optimizer, loss_value)
-
-    error_norm = torch.sqrt(torch.sum(V.integrate_functional(h1_norm))) / exact_norm
-
-    relative_loss = torch.sqrt(loss_value) / exact_norm
-
-    print(
-        f"Loss: {loss_value.item():.8f} Relative Loss: {relative_loss.item():.8f} Relative error: {error_norm.item():.8f}"
+def training_step(neural_network):
+    """Training step for the neural network."""
+    residual_vector = discrete_basis.reduce(
+        discrete_basis.integrate_linear_form(residual, neural_network.gradient)
     )
 
-    if loss_value < LOSS_OPT:
-        LOSS_OPT = loss_value
-        PARAMS_OPT = NN.state_dict()
+    # loss_value = residual_vector.T @ (gram_matrix_inverse @ residual_vector)
 
-    loss_list.append(loss_value.item())
-    relative_loss_list.append(relative_loss.item())
-    H1_error_list.append(error_norm.item())
+    loss_value = torch.sum(residual_vector**2, dim=0)
 
-end_time = datetime.now()
+    relative_loss = torch.sqrt(loss_value) / exact_norm**2
 
-execution_time = end_time - start_time
+    h1_error = torch.sqrt(
+        torch.sum(
+            discrete_basis.integrate_functional(
+                h1_norm, neural_network, neural_network.gradient
+            )
+        )
+    )
 
-print(f"Training time: {execution_time}")
+    return loss_value, relative_loss, h1_error / exact_norm
+
+
+model = Model(
+    neural_network=NN,
+    training_step=training_step,
+    epochs=5000,
+    learning_rate=0.1e-1,
+    use_decay_learning_rate=True,
+    decay_rate=0.99,
+    decay_steps=100,
+    use_early_stopping=True,
+    early_stopping_patience=100,
+    min_delta=1e-12,
+)
+
+model.train()
 
 # ---------------------- Plotting ----------------------#
 
-NN.load_state_dict(PARAMS_OPT)
+model.load_optimal_parameters()
 
-NB_PLOT_POINTS = 100
+linspace = torch.linspace(0, 1, 100)
 
-X, Y = torch.meshgrid(
-    torch.linspace(0, 1, NB_PLOT_POINTS),
-    torch.linspace(0, 1, NB_PLOT_POINTS),
-    indexing="ij",
+X, Y = torch.stack(
+    torch.meshgrid(
+        linspace,
+        linspace,
+        indexing="ij",
+    )
 )
+
+plot_points = torch.stack([X, Y], dim=-1)
 
 with torch.no_grad():
-    Z = abs(exact(X, Y) - NN(X, Y))
+    Z = abs(exact(X, Y) - NN(plot_points).squeeze(-1))
 
 figure_solution, axis_solution = plt.subplots()
+contour_solution = axis_solution.contourf(
+    X.numpy(force=True),
+    Y.numpy(force=True),
+    Z.numpy(force=True),
+    levels=100,
+    cmap="viridis",
+)
+figure_solution.colorbar(contour_solution, ax=axis_solution, orientation="vertical")
 
-fig, ax = plt.subplots(dpi=500)
-c = ax.contourf(X.cpu(), Y.cpu(), Z.cpu(), levels=100, cmap="viridis")
-fig.colorbar(c, ax=ax, orientation="vertical")
-
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_title(r"$|u-u_\theta|$")
+axis_solution.set_xlabel("x")
+axis_solution.set_ylabel("y")
+axis_solution.set_title(r"$|u-u_\theta|$")
 plt.tight_layout()
 
-figure_error, axis_error = plt.subplots(dpi=500)
-
-axis_error.semilogy(
-    relative_loss_list,
-    label=r"$\frac{\sqrt{\mathcal{L}(u_\theta)}}{\|u\|_{U}}$",
-    linestyle="-.",
-)
-
-axis_error.semilogy(
-    H1_error_list, label=r"$\frac{\|u-u_\theta\|_{U}}{\|u\|_{U}}$", linestyle=":"
-)
-
-axis_error.legend(fontsize=15)
-
-figure_loglog, axis_loglog = plt.subplots()
-
-axis_loglog.loglog(relative_loss_list, H1_error_list)
-
-axis_loglog.set(
-    title="Error vs Loss comparison of RVPINNs method",
-    xlabel="Relative Loss",
-    ylabel="Relative Error",
-)
+model.plot_training_history()
 
 plt.show()
