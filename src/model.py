d6069c09db0e6684d685b3498756ddd2862a2ecc
Nicolas Zamorano <nicolas.zamo12@gmail.com>
2025-09-16 21:10:09 +0200

    add TorchScript implementaion for NN in Model Class
    to speed up traning. Laplacian need to be refacture to work again

--- src/model.py
+++ src/model.py
@@ -1,23 +1,32 @@
 "# Definition of a Deep Neural Network (DNN) model using PyTorch."
 
+from typing import List, Optional
+
+import matplotlib.pyplot as plt
 import torch
 import tqdm
 
-import matplotlib.pyplot as plt
+
+class IdentityBC(torch.nn.Module):
+    """base class for strong application of  boundary conditions"""
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forward pass"""
+        return torch.ones_like(x[..., :1])
 
 
 class FeedForwardNeuralNetwork(torch.nn.Module):
-    """Feed-Forward Neural Network (FNN) class."""
+    """Feed-Forward Neural Network (FNN) class compatible with TorchScript."""
 
     def __init__(
         self,
-        input_dimension,
-        output_dimension,
-        nb_hidden_layers,
-        neurons_per_layers,
-        activation_function=torch.nn.Tanh(),
-        use_xavier_initialization=False,
-        boundary_condition_modifier=lambda x: 1,
+        input_dimension: int,
+        output_dimension: int,
+        nb_hidden_layers: int,
+        neurons_per_layers: int,
+        activation_function: torch.nn.Module = torch.nn.Tanh(),
+        use_xavier_initialization: bool = False,
+        boundary_condition_modifier: torch.nn.Module = None,
     ):
         super().__init__()
         self._input_dimension = input_dimension
@@ -26,7 +35,11 @@ class FeedForwardNeuralNetwork(torch.nn.Module):
         self._neurons_per_layers = neurons_per_layers
         self._activation_function = activation_function
         self._use_xavier_initialization = use_xavier_initialization
-        self._boundary_condition_modifier = boundary_condition_modifier
+
+        if boundary_condition_modifier is None:
+            self._boundary_condition_modifier = IdentityBC()
+        else:
+            self._boundary_condition_modifier = boundary_condition_modifier
 
         self._neural_network = self.build_network(
             input_dimension,
@@ -45,7 +58,7 @@ class FeedForwardNeuralNetwork(torch.nn.Module):
         neurons_per_layers: int,
         activation_function: torch.nn.Module,
         use_xavier_initialization: bool,
-    ):
+    ) -> torch.nn.Sequential:
         """Build the neural network architecture."""
         layers = []
 
@@ -58,55 +71,68 @@ class FeedForwardNeuralNetwork(torch.nn.Module):
 
         layers.append(torch.nn.Linear(neurons_per_layers, output_dimension))
 
+        seq = torch.nn.Sequential(*layers)
+
         if use_xavier_initialization:
-            for layer in layers:
+            for layer in seq:
                 if isinstance(layer, torch.nn.Linear):
                     torch.nn.init.xavier_uniform_(layer.weight)
 
-        return torch.nn.Sequential(*layers)
+        return seq
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass through the network."""
         return self._neural_network(x) * self._boundary_condition_modifier(x)
 
-    def gradient(self, inputs: torch.Tensor):
+    @torch.jit.export
+    def gradient(self, inputs: torch.Tensor) -> Optional[torch.Tensor]:
         """Compute the gradient of the neural network with respect to its inputs."""
         inputs.requires_grad_(True)
         output = self.forward(inputs)
+
+        grad_outputs: List[Optional[torch.Tensor]] = [torch.ones_like(output)]
+
         gradients = torch.autograd.grad(
-            outputs=output,
-            inputs=inputs,
-            grad_outputs=torch.ones_like(output),
+            outputs=[output],
+            inputs=[inputs],
+            grad_outputs=grad_outputs,
             retain_graph=True,
             create_graph=True,
         )[0]
+
         return gradients
 
-    def laplacian(self, inputs: torch.Tensor):
-        """Compute the laplacian of the neural network with respect to its inputs."""
-        inputs.requires_grad_(True)
-        output = self.forward(inputs)
-        gradients = torch.autograd.grad(
-            outputs=output,
-            inputs=inputs,
-            grad_outputs=torch.ones_like(output),
-            retain_graph=True,
-            create_graph=True,
-        )[0]
+    # @torch.jit.export
+    # def laplacian(self, inputs: torch.Tensor) -> torch.Tensor:
+    #     """Compute the laplacian of the neural network with respect to its inputs."""
+    #     inputs.requires_grad_(True)
+    #     output = self.forward(inputs)
 
-        laplacian = torch.zeros_like(output)
+    #     grad_outputs: List[Optional[torch.Tensor]] = [torch.ones_like(output)]
 
-        for i in range(inputs.size(-1)):
-            grad2 = torch.autograd.grad(
-                gradients[..., [i]],
-                inputs,
-                grad_outputs=torch.ones_like(gradients[..., [i]]),
-                create_graph=True,
-                retain_graph=True,
-            )[0][..., [i]]
-            laplacian += grad2
+    #     gradients = torch.autograd.grad(
+    #         outputs=[output],
+    #         inputs=[inputs],
+    #         grad_outputs=grad_outputs,
+    #         retain_graph=True,
+    #         create_graph=True,
+    #     )[0]
 
-        return laplacian
+    #     laplacian = torch.zeros_like(output)
+
+    #     for i in range(inputs.size(-1)):
+    #         gradient = gradients.index_select(-1, torch.tensor([i]))
+    #         grad_outputs: List[Optional[torch.Tensor]] = [torch.ones_like(gradient)]
+    #         grad2 = torch.autograd.grad(
+    #             [gradient],
+    #             [inputs],
+    #             grad_outputs=grad_outputs,
+    #             create_graph=True,
+    #             retain_graph=True,
+    #         )[0][..., i : i + 1]
+    #         laplacian += grad2
+
+    #     return laplacian
 
 
 class Model:
@@ -125,7 +151,7 @@ class Model:
         early_stopping_patience: int = 10,
         min_delta: float = 1e-12,
     ):
-        self._neural_network = neural_network
+        self._neural_network = torch.jit.script(neural_network)
         self._training_step = training_step
         self._epochs = epochs
         if optimizer_kwargs is None:
