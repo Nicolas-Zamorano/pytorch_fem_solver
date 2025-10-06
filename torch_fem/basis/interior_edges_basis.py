c5deb99a76297dbc3dd8b2e6bf3250fd300704cd
Nicolas Zamorano <nicolas.zamo12@gmail.com>
2025-10-06 15:31:25 +0200

    bugfix _compute_basis_parameters for InteriorEdgesBasis
    as was returning a tensordict and shapes net to be tuple of ints

--- torch_fem/basis/interior_edges_basis.py
+++ torch_fem/basis/interior_edges_basis.py
@@ -49,16 +49,14 @@ class InteriorEdgesBasis(AbstractBasis):
 
         form_idx = global_dofs4elements.reshape(-1)
 
-        return tensordict.TensorDict(
-            {
-                "bilinear_form_shape": (nb_global_dofs, nb_global_dofs),
-                "bilinear_form_idx": (rows_idx, cols_idx),
-                "linear_form_shape": (nb_global_dofs, 1),
-                "linear_form_idx": (form_idx,),
-                "inner_dofs": inner_dofs,
-                "nb_dofs": nb_global_dofs,
-            }
-        )
+        return {
+            "bilinear_form_shape": (nb_global_dofs, nb_global_dofs),
+            "bilinear_form_idx": (rows_idx, cols_idx),
+            "linear_form_shape": (nb_global_dofs, 1),
+            "linear_form_idx": (form_idx,),
+            "inner_dofs": inner_dofs,
+            "nb_dofs": nb_global_dofs,
+        }
 
     def _compute_jacobian_map(self, mesh, element):
         return mesh["interior_edges", "coordinates"].mT @ element.barycentric_grad
