46f2c2eb5931197b03af89f46cee15adad20bf0d
Nicolas Zamorano <nicolas.zamo12@gmail.com>
2025-11-27 15:31:55 +1100

    update for 3D problems

--- torch_fem/problems/abstract_problem.py
+++ torch_fem/problems/abstract_problem.py
@@ -26,15 +26,20 @@ class AbstractProblem(abc.ABC):
         """Exact solution derivative with respect to z."""
         raise NotImplementedError
 
+    def dirichlet_boundary(self, coordinates: torch.Tensor) -> torch.Tensor:
+        """Dirichlet boundary condition function."""
+        raise NotImplementedError
+
     def precomputed_H1_norm(
         self,
         _,
         value: torch.Tensor,
         value_dx: torch.Tensor,
         value_dy: torch.Tensor,
+        value_dz: torch.Tensor = torch.tensor(0.0),
     ) -> torch.Tensor:
         """H1 norm for precomputed values."""
-        return value**2 + value_dx**2 + value_dy**2
+        return value**2 + value_dx**2 + value_dy**2 + value_dz**2
 
     def precomputed_L2_norm(
         self,
@@ -44,13 +49,25 @@ class AbstractProblem(abc.ABC):
         """L2 norm for precomputed values."""
         return value**2
 
-    def H1_norm(self, basis, function, function_dx, function_dy) -> torch.Tensor:
+    def H1_norm(
+        self, basis, function, function_dx, function_dy, function_dz=None
+    ) -> torch.Tensor:
         """H1 norm."""
         function_value = function(basis.integration_points)
         function_dx_value = function_dx(basis.integration_points)
         function_dy_value = function_dy(basis.integration_points)
 
-        return function_value**2 + function_dx_value**2 + function_dy_value**2
+        if function_dz is not None:
+            function_dz_value = function_dz(basis.integration_points)
+        else:
+            function_dz_value = 0.0
+
+        return (
+            function_value**2
+            + function_dx_value**2
+            + function_dy_value**2
+            + function_dz_value**2
+        )
 
     def L2_norm(self, basis, function) -> torch.Tensor:
         """L2 norm."""
@@ -61,32 +78,33 @@ class AbstractProblem(abc.ABC):
     @staticmethod
     @abc.abstractmethod
     def bilinear_form(basis) -> torch.Tensor:
+        """Bilinear form of the PDE."""
         raise NotImplementedError
 
     @staticmethod
     @abc.abstractmethod
-    def linear_form(basis, rhs_values: torch.Tensor) -> torch.Tensor:
+    def linear_form(basis, rhs_value: torch.Tensor) -> torch.Tensor:
+        """Linear form of the PDE."""
         raise NotImplementedError
 
     @staticmethod
     @abc.abstractmethod
     def residual(
-        basis, gradient: torch.Tensor, rhs_values: torch.Tensor
+        basis, gradient: torch.Tensor, rhs_value: torch.Tensor
     ) -> torch.Tensor:
+        """Residual form of the PDE."""
         raise NotImplementedError
 
     @staticmethod
     def bulk_residual(
-        basis, laplcian: torch.Tensor, rhs_values: torch.Tensor
+        basis, laplacian: torch.Tensor, rhs_value: torch.Tensor
     ) -> torch.Tensor:
+        """Bulk residual of the PDE."""
         raise NotImplementedError
 
     @staticmethod
     def jump_residual(
         edges_basis, gradient_for_jump: torch.Tensor, normals_4_elements: torch.Tensor
     ) -> torch.Tensor:
-        raise NotImplementedError
-
-    @staticmethod
-    def dirichlet_boundary(coordinates: torch.Tensor) -> torch.Tensor:
+        """Jump residual of the PDE."""
         raise NotImplementedError
